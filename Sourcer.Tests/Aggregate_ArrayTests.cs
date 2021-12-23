bd197b2fc6263cb622007b2c8e3420f2fe2cf905
Lejdholt <fredrik.lejdholt@gmail.com>
2021-12-23 20:07:47 +0100

    array tests

---
+++ Sourcer.Tests/Aggregate_ArrayTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sourcer.Tests;
+
+public class Aggregate_ArrayTests
+{
+    [Fact(DisplayName = "Given property with array when new command with larger array then prioritize should return larger array")]
+    public void NewLargerArray()
+    {
+        var agg = new Aggregate();
+
+        agg.ApplySource(new SourceCommand("id1", "source1", "{\"Names\":[\"Name1\"]}"));
+        agg.ApplySource(new SourceCommand("id1", "source1", "{\"Names\":[\"Name1\",\"Name2\"]}"));
+
+        var result = agg.Prioritize("{\"default\":{}}");
+
+        result.Should().Be("{\"Names\":[\"Name1\",\"Name2\"]}");
+    } 
+    
+    [Fact(DisplayName = "Given property with array when new command with smaller array then prioritize should return smaller array")]
+    public void NewSmallerArray()
+    {
+        var agg = new Aggregate();
+
+        agg.ApplySource(new SourceCommand("id1", "source1", "{\"Names\":[\"Name1\",\"Name2\"]}"));
+        agg.ApplySource(new SourceCommand("id1", "source1", "{\"Names\":[\"Name1\"]}"));
+      
+
+        var result = agg.Prioritize("{\"default\":{}}");
+
+        result.Should().Be("{\"Names\":[\"Name1\"]}");
+    }
+}
